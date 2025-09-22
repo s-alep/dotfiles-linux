0a66f989b4f700693d56c99bf7afc9f0e43155cc
Spyros Alepakos <spyr1996@gmail.com>
2025-09-23 01:09:58 +0300

    obsivim lualine

--- arch/obsivim/.config/obsivim/lua/plugins/lualine.lua
+++ arch/obsivim/.config/obsivim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
 				icons_enabled = true,
 				theme = "auto",
 				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
