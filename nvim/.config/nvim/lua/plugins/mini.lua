84004910e9acb8e8c693e5b64404bbec0e8e09d8
Spyros Alepakos <spyr1996@gmail.com>
2025-11-23 06:39:56 +0200

    123

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,44 +1,48 @@
 return {
-  {'nvim-mini/mini.extra', version = false, config = function()  require('mini.extra').setup()end,},
-  {
-    'nvim-mini/mini.ai',
-    version = '*',
-    config = function()
-      local treesitter = require('mini.ai').gen_spec.treesitter
-      local ai = require('mini.extra').gen_ai_spec
-      require('mini.ai').setup {
-        n_lines = 500,
-        custom_textobjects = {
-          f = treesitter { a = '@function.outer', i = '@function.inner' },
-          c = treesitter { a = '@class.outer', i = '@class.inner' },
-          i = ai.indent(),
-          B = ai.buffer(),
-          g = ai.line(),
-        },
-      }
-    end,
-  },
-  {'nvim-mini/mini.icons', version = '*', config = function()   require('mini.icons').setup {}end,},
-  {
-    'nvim-mini/mini.indentscope',
-    version = false,
-    config = function()
-      require('mini.indentscope').setup {
-        draw = { delay = 0, animation = require('mini.indentscope').gen_animation.none() },
-        options = { try_as_border = true },
-        symbol = '│',
-      }
-    end,
-  },
-  {
-    'nvim-mini/mini.splitjoin',
-    version = '*',
-    config = function()
-      require('mini.splitjoin').setup {
-        mappings = { toggle = 'grs', split = '', join = '' }
-      }
-    end,
-  },
-  {'nvim-mini/mini.surround', version = '*', config = function()   require('mini.surround').setup {}end,},
-  {'nvim-mini/mini.pairs', version = '*', config = function()   require('mini.pairs').setup()end }
+    {'nvim-mini/mini.extra', version = false, config = function()  require('mini.extra').setup()end,},
+    {
+        'nvim-mini/mini.ai',
+        version = '*',
+        config = function()
+            local treesitter = require('mini.ai').gen_spec.treesitter
+            local ai = require('mini.extra').gen_ai_spec
+            require('mini.ai').setup {
+                n_lines = 500,
+                custom_textobjects = {
+                    f = treesitter { a = '@function.outer', i = '@function.inner' },
+                    c = treesitter { a = '@class.outer', i = '@class.inner' },
+                    i = ai.indent(),
+                    B = ai.buffer(),
+                    g = ai.line(),
+                },
+            }
+        end,
+    },
+    {'nvim-mini/mini.icons', version = '*', config = function()   require('mini.icons').setup {}end,},
+    { 'nvim-mini/mini.diff', version = false, config = function()
+        require('mini.diff').setup{}
+        vim.keymap.set('n', '<leader>gd', '<cmd>lua  MiniDiff.toggle_overlay()<cr>')
+    end},
+    {
+        'nvim-mini/mini.indentscope',
+        version = false,
+        config = function()
+            require('mini.indentscope').setup {
+                draw = { delay = 0, animation = require('mini.indentscope').gen_animation.none() },
+                options = { try_as_border = true },
+                symbol = '│',
+            }
+        end,
+    },
+    {
+        'nvim-mini/mini.splitjoin',
+        version = '*',
+        config = function()
+            require('mini.splitjoin').setup {
+                mappings = { toggle = 'grs', split = '', join = '' }
+            }
+        end,
+    },
+    {'nvim-mini/mini.surround', version = '*', config = function()   require('mini.surround').setup {}end,},
+    {'nvim-mini/mini.pairs', version = '*', config = function()   require('mini.pairs').setup()end }
 }
