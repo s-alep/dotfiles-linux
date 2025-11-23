209e1eded1e2c486bf998c400c5c87c395a488c5
Spyros Alepakos <spyr1996@gmail.com>
2025-11-23 06:40:10 +0200

    123

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local font = wezterm.font
 
+local toggle_terminal = wezterm.plugin.require("https://github.com/zsh-sage/toggle_terminal.wez")
+
 config.use_fancy_tab_bar = false
 config.default_prog = { 'nu'}
 config.tab_max_width = 20
@@ -19,7 +21,7 @@ config.hide_mouse_cursor_when_typing = true
 config.warn_about_missing_glyphs = false
 config.window_background_opacity = 1.0
 config.color_scheme = 'zenwritten_dark'
--- config.color_scheme = 'MaterialDarker'
+config.color_scheme = 'Ef-Autumn'
 config.inactive_pane_hsb = {saturation = 1.0, brightness = 1.0,}
 config.window_padding = {left = "0", right = "0", top = "0", bottom = "0",}
 config.colors = {tab_bar = { background = "none" },}
@@ -54,4 +56,16 @@ wezterm.on('augment-command-palette', function(window, pane)
 	return require('command_palette')
 end)
 
+toggle_terminal.apply_to_config(config, {
+	key = ";", -- Key for the toggle action
+	mods = "LEADER", -- Modifier keys for the toggle action
+	direction = "Down", -- Direction to split the pane
+	size = { Percent = 40 }, -- Size of the split pane
+	change_invoker_id_everytime = false, -- Change invoker pane on every toggle
+	zoom = {
+		auto_zoom_toggle_terminal = false, -- Automatically zoom toggle terminal pane
+		auto_zoom_invoker_pane = true, -- Automatically zoom invoker pane
+		remember_zoomed = true, -- Automatically re-zoom the toggle pane if it was zoomed before switching away
+	}
+})
 return config
